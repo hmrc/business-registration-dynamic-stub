0126c83d9d5b1405f9a3c57159a5c2d66a5841a2
david.greatorex <david.greatorex@digital.hmrc.gov.uk>
2016-11-02 17:27:29 +0000

    Fixes to json validation

--- app/controllers/StubController.scala
+++ app/controllers/StubController.scala
@@ -43,7 +43,7 @@ trait StubController extends BaseController with ServicesConfig {
     implicit request =>
       Try(request.body.validate[FullDesSubmission]) match {
         case Success(JsSuccess(_, _)) => Future.successful(Ok(Json.toJson(successDesResponse)))
-        case Success(JsError(_)) => Future.successful(BadRequest(Json.toJson(invalidJsonResponse)))
+        case Success(JsError(errors)) => Future.successful(BadRequest(Json.toJson(s"$invalidJsonResponse - errors: ${errors.toString()}")))
         case Failure(e) => Future.successful(BadRequest(Json.toJson(malformedJsonResponse)))
       }
   }
