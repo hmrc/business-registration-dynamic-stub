5e66e3b924a93218845157aca9d61ba9a424b8ab
Michele Satta <3033136+msatta@users.noreply.github.com>
2025-11-12 09:10:32 +0000

    [DL-17711] Address jackson-core vulnerability (#49)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,14 +5,15 @@ import sbt.*
 object AppDependencies {
 
   private val playVersion                   =  "-play-30"
-  private val bootstrapPlayVersion          =  "8.6.0"
+  private val bootstrapPlayVersion          =  "10.4.0"
   private val scalaTestVersion              =  "3.2.19"
-  private val scalaTestPlusPlayVersion      =  "7.0.1"
+  private val scalaTestPlusPlayVersion      =  "7.0.2"
   private val flexmarkVersion               =  "0.64.8"
-  private val wireMockVersion               =  "3.13.0"
-  private val hmrcMongoVersion              =  "2.6.0"
+  private val wireMockVersion               =  "3.13.1"
+  private val hmrcMongoVersion              =  "2.10.0"
   private val catsVersion                   =  "2.13.0"
-  private val playTestVersion               =  "3.0.7"
+  private val playTestVersion               =  "3.0.9"
+  private val mockitoVersion                =  "3.2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
@@ -28,7 +29,7 @@ object AppDependencies {
     "org.scalatestplus.play"    %%  "scalatestplus-play"              %   scalaTestPlusPlayVersion  % Test,
     "org.playframework"         %%  "play-test"                       %   playTestVersion           % Test,
     "com.vladsch.flexmark"      %   "flexmark-all"                    %   flexmarkVersion           % Test,
-    "org.scalatestplus"         %%  "mockito-4-5"                     % "3.2.12.0"      % Test,
+    "org.scalatestplus"         %%  "mockito-4-5"                     %   mockitoVersion            % Test,
     "org.wiremock"              %   "wiremock-standalone"             %   wireMockVersion           % Test
   )
 
