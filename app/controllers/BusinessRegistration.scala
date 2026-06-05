fbed4f66f54f9ae134f3bb3d0dbe8ff022abcba0
Murali raju <17339891+muralishub@users.noreply.github.com>
2026-06-05 12:06:55 +0100

    DM-20: define controllers and endpoints with static reponse

---
+++ app/controllers/BusinessRegistration.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import cats.instances.FutureInstances
+import com.google.inject.Singleton
+import play.api.Logger
+import play.api.libs.json.Json
+import play.api.mvc.ControllerComponents
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class BusinessRegistration @Inject()( cc: ControllerComponents)(implicit val ec: ExecutionContext)  extends BackendController(cc) with FutureInstances {
+
+  val logger: Logger = Logger(this.getClass())
+  def register(regime: String) = Action { request =>
+    logger.info(s"[StubController] [topup] Received topup containing: ${request.body}")
+    Accepted(Json.obj("processingDate" -> "2015-12-17T09:30:47Z", "acknowledgementReference" -> "SCRS01234567890"))
+  }
+}
